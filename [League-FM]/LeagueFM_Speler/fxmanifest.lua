48d8209b89bd0b22284b07c7097b2e906259688d
rich1521 <77267602+rich1521@users.noreply.github.com>
2024-11-15 12:38:23 +0100

    Versie checker toegevoegd

--- [League-FM]/LeagueFM_Speler/fxmanifest.lua
+++ [League-FM]/LeagueFM_Speler/fxmanifest.lua
@@ -1,8 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
-author 'Richard | Manager'
+author 'LeagueFM | rich1521'
 description 'Dit script geeft jouw in game toegang tot onze player.'
-version '0.0.1'
+version '0.0.2'
+versioncheck "https://raw.githubusercontent.com/rich1521/League-FM/refs/heads/main/%5BLeague-FM%5D/LeagueFM_Speler/fxmanifest.lua"
 lua54 'yes'
 
 ui_page 'player/index.html'
